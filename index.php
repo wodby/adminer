74d48f1a71e572cb52e1c1d09953f665443cdfb0
Pavel Prischepa <pprischepa@gmail.com>
2017-01-10 12:59:56 +0700

    Added ADMINER_SALT

--- index.php
+++ index.php
@@ -3,7 +3,21 @@
 function adminer_object() {
 
   class AdminerSoftware extends Adminer {
-    // Place Adminer modifications here.
+
+    /**
+     * Get key used for permanent login
+     * @param bool
+     * @return string
+     */
+    function permanentLogin($create = false) {
+      if ($key = getenv('ADMINER_SALT')) {
+        return $key;
+      }
+      else {
+        return parent::permanentLogin($create);
+      }
+    }
+
   }
 
   return new AdminerSoftware;
