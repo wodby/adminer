9b68c678fda42c26ab9f26d71e49b3970bd9ff0e
Chingis Sandanov <chingis@wodby.com>
2017-06-27 20:03:15 +0700

    Remove old overridden options

--- 4.3/index.php
+++ 4.3/index.php
@@ -1,22 +1,5 @@
 <?php
 
-$adminer_ssid = getenv('ADMINER_SESSID');
-
-if (!$adminer_ssid && getenv('WODBY_INSTANCE_UUID')) {
-  $adminer_ssid = str_replace('-', '', getenv('WODBY_INSTANCE_UUID'));
-}
-
-if (!defined("SID") && $adminer_ssid) {
-  session_name("adminer_sid_$adminer_ssid"); // use specific session name to get own namespace
-  $HTTPS = !empty($_SERVER["HTTPS"]) && strcasecmp($_SERVER["HTTPS"], "off");
-  $params = array(0, preg_replace('~\\?.*~', '', $_SERVER["REQUEST_URI"]), "", $HTTPS);
-  if (version_compare(PHP_VERSION, '5.2.0') >= 0) {
-    $params[] = true; // HttpOnly
-  }
-  call_user_func_array('session_set_cookie_params', $params); // ini_set() may be disabled
-  session_start();
-}
-
 function adminer_object() {
 
   class AdminerSoftware extends Adminer {
